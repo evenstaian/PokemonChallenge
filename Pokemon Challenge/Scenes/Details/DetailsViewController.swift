b348bc05e9b5e07cc29ed3454c8670c1ad849862
evenstaian <evenstaian@gmail.com>
2025-01-19 16:50:03 -0300

    init initial project structure and architeture

---
+++ Pokemon Challenge/Scenes/Details/DetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.swift
+//  Pokemon Challenge
+//
+//  Created by Evens Taian on 19/01/25.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Pokemon Challenge/Scenes/Pokemons/PokemonsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonsViewController.swift
+//  Pokemon Challenge
+//
+//  Created by Evens Taian on 19/01/25.
+//
+
+import UIKit
+
+class PokemonsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+    
+}
