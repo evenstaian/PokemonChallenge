8d1e396192759e20316ddd5ae1678864f234d32a
evenstaian <evenstaian@gmail.com>
2025-01-19 15:05:10 -0300

    Initial Commit

---
+++ Pokemon Challenge/Pokemon_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_ChallengeApp.swift
+//  Pokemon Challenge
+//
+//  Created by Evens Taian on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
