3b0024b8db52bb15916137cd005f01afbab794e1
evenstaian <evenstaian@gmail.com>
2025-01-20 09:29:42 -0300

    create custom launch screen

--- Pokemon Challenge/Application/SceneDelegate.swift
+++ Pokemon Challenge/Application/SceneDelegate.swift
@@ -9,16 +9,35 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    private var mainNavigation: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
+
         let rootScene = PokemonsFactory.makeModule()
         let navigation = UINavigationController(rootViewController: rootScene)
+        self.mainNavigation = navigation
         
-        window.rootViewController = navigation
+        let launchScreen = LaunchScreenViewController()
+        launchScreen.onFinishAnimation = { [weak self] in
+            self?.showMainScreen()
+        }
+        
+        window.rootViewController = launchScreen
         window.makeKeyAndVisible()
         self.window = window
+    }
+    
+    private func showMainScreen() {
+        guard let navigation = mainNavigation else { return }
+        
+        UIView.transition(with: window!,
+                         duration: 0.5,
+                         options: .transitionCrossDissolve,
+                         animations: {
+            self.window?.rootViewController = navigation
+        })
     }
 } 
