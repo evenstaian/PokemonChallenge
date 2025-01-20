899d99fe971bb1920ab3cb500ad4a38ba64fc696
evenstaian <evenstaian@gmail.com>
2025-01-20 16:30:41 -0300

    create orientation manager and implement on views

--- Pokemon Challenge/Scenes/Details/DetailsViewController.swift
+++ Pokemon Challenge/Scenes/Details/DetailsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class DetailsViewController: UIViewController {
     
     private let detailsViewModel: DetailsViewmodeling
+    private var portraitConstraints : [NSLayoutConstraint]?
+    private var landscapeConstraints : [NSLayoutConstraint]?
     
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
@@ -67,6 +69,16 @@ class DetailsViewController: UIViewController {
             self?.evolutionChainComponent.configure(with: evolutionChainDetails.chain)
         }
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if let portraitConstraints = portraitConstraints, let landscapeConstraints = landscapeConstraints {
+            coordinator.animate { [weak self] _ in
+                self?.updateConstraintsWithOrientation(portraitConstraints, landscapeConstraints)
+            }
+        }
+    }
 }
 
 extension DetailsViewController: ViewCode {
@@ -82,7 +94,7 @@ extension DetailsViewController: ViewCode {
     }
     
     func setupConstraints() {
-        NSLayoutConstraint.activate([
+        portraitConstraints = [
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -107,6 +119,38 @@ extension DetailsViewController: ViewCode {
             evolutionChainComponent.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             evolutionChainComponent.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             evolutionChainComponent.heightAnchor.constraint(equalToConstant: 180)
-        ])
+        ]
+        
+        landscapeConstraints = [
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            headerComponent.topAnchor.constraint(equalTo: contentView.topAnchor),
+            headerComponent.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            headerComponent.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5),
+            
+            evolutionChainComponent.topAnchor.constraint(equalTo: headerComponent.bottomAnchor, constant: 16),
+            evolutionChainComponent.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            evolutionChainComponent.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5),
+            evolutionChainComponent.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor),
+            evolutionChainComponent.heightAnchor.constraint(equalToConstant: 180),
+            
+            attributesComponent.topAnchor.constraint(equalTo: contentView.topAnchor),
+            attributesComponent.leadingAnchor.constraint(equalTo: headerComponent.trailingAnchor, constant: 16),
+            attributesComponent.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            attributesComponent.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        
+        if let _ = portraitConstraints, let _ = landscapeConstraints {
+            updateConstraintsWithOrientation(portraitConstraints!, landscapeConstraints!)
+        }
     }
 }
--- Pokemon Challenge/Scenes/Pokemons/PokemonsViewController.swift
+++ Pokemon Challenge/Scenes/Pokemons/PokemonsViewController.swift
@@ -12,6 +12,9 @@ class PokemonsViewController: UIViewController {
     private let viewModel: PokemonsViewmodeling
     private let pokemonsCollectionDataSource = PokemonCollectionDataSource()
     
+    private var portraitConstraints : [NSLayoutConstraint]?
+    private var landscapeConstraints : [NSLayoutConstraint]?
+    
     private lazy var headerComponent: HeaderComponent = {
         let header = HeaderComponent()
         header.translatesAutoresizingMaskIntoConstraints = false
@@ -50,6 +53,16 @@ class PokemonsViewController: UIViewController {
         self.viewModel.viewDidLoad()
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if let portraitConstraints = portraitConstraints, let landscapeConstraints = landscapeConstraints {
+            coordinator.animate { [weak self] _ in
+                self?.updateConstraintsWithOrientation(portraitConstraints, landscapeConstraints)
+            }
+        }
+    }
+    
     private func setupClicks() {
         pokemonsCollectionDataSource.didClick = { pokemon in
             self.viewModel.goToDetails(pokemon: pokemon)
@@ -77,7 +90,7 @@ extension PokemonsViewController: ViewCode {
     }
     
     func setupConstraints() {
-        NSLayoutConstraint.activate([
+        portraitConstraints = [
             headerComponent.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerComponent.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerComponent.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -86,6 +99,22 @@ extension PokemonsViewController: ViewCode {
             listComponent.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             listComponent.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             listComponent.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+        ]
+        
+        landscapeConstraints = [
+            headerComponent.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerComponent.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerComponent.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            headerComponent.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5), // Header ocupará 40% da largura
+            
+            listComponent.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            listComponent.leadingAnchor.constraint(equalTo: headerComponent.trailingAnchor),
+            listComponent.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            listComponent.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        
+        if let _ = portraitConstraints, let _ = landscapeConstraints {
+            updateConstraintsWithOrientation(portraitConstraints!, landscapeConstraints!)
+        }
     }
 }
